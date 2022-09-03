88350dfc3fc0b4c951e122c2ce8853fd3364b0bf
mpilopillz <mpilo.dlamini@yahoo.com>
2022-09-03 18:45:52 +0200

    Initial Commit

---
+++ I Am Fika/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Fika
+//
+//  Created by Mpilo Pillz on 2022/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
