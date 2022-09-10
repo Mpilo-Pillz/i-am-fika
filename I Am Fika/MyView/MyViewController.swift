be35c3a492bd9c0e21c14e580f31264ad84e6740
mpilopillz <mpilo.dlamini@yahoo.com>
2022-09-10 15:55:48 +0200

    refactoring

---
+++ I Am Fika/MyView/MyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyViewController.swift
+//  I Am Fika
+//
+//  Created by Mpilo Pillz on 2022/09/10.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+    let contentView: UIView = {
+        let view = UIView()
+        
+        return view
+    }()
+    
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        view.backgroundColor = .green
+        setUpContentView()
+    }
+    
+   
+    
+
+   
+}
--- I Am Fika/MyView/MyViewControllerExtension.swift
+++ I Am Fika/MyView/MyViewControllerExtension.swift
@@ -1,5 +1,5 @@
 //
-//  MyViewController.swift
+//  MyScreenView.swift
 //  I Am Fika
 //
 //  Created by Mpilo Pillz on 2022/09/10.
@@ -7,43 +7,36 @@
 
 import UIKit
 
-class MyViewController: UIViewController {
-    let contentView = UIView()
-    
-    
-    let titleLabel = UILabel()
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+extension MyViewController {
 
-//        view.backgroundColor = .green
-        setUpContentView()
+    func setUpContentView() {
+       
+        setUpContentViewConstraints()
+        setUpTitleLabel()
     }
     
-    func setUpContentView() {
+    func setUpTitleLabel() {
         
         titleLabel.text = "I am Fikasentani"
         
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 2).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 2).isActive = true
+       
+    }
+    
+    func setUpContentViewConstraints() {
         view.addSubview(contentView)
+        view.backgroundColor = .cyan
         contentView.addSubview(titleLabel)
-        
-        
-        
         contentView.translatesAutoresizingMaskIntoConstraints = false
         
         contentView.heightAnchor.constraint(equalToConstant: view.frame.height / 3).isActive = true
         contentView.topAnchor.constraint(equalTo: view.topAnchor, constant: 45).isActive = true
         contentView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 5).isActive = true
         contentView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -5).isActive = true
-        
-        contentView.backgroundColor = .cyan
-        
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 2).isActive = true
-        titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 2).isActive = true
     }
-    
 
-   
 }
+
+
