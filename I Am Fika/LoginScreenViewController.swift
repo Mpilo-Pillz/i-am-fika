dca744c9ead95c1cce22ea88374f4a790f035159
mpilopillz <mpilo.dlamini@yahoo.com>
2022-09-10 08:57:54 +0200

    adding elemebts but not showing on the screen

--- I Am Fika/LoginScreenViewController.swift
+++ I Am Fika/LoginScreenViewController.swift
@@ -8,9 +8,40 @@
 import UIKit
 
 class LoginScreenViewController: UIViewController {
-        
+    
+    let loginContentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "EMAIL"
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "PASSWORD (MIN. 10 CHARACTERS)"
+        return textField
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.addTarget(self, action: #selector(loginButtonPressed), for: UIControl.Event.touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+    
+    @objc func loginButtonPressed(sender: UIButton) {
+        print("PRESSED")
+        loginContentView.backgroundColor = . yellow
     }
 }
 
